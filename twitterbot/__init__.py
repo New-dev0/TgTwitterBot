77bacbe4c6c5d2aec5f4ac262c5cf63abba0befa
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-29 14:56:15 +0530

    Update __init__.py

--- twitterbot/__init__.py
+++ twitterbot/__init__.py
@@ -9,7 +9,7 @@ from Configs import Var
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton as Button
 
 
-AUTH = list(set(int(a) for a in Var.AUTHUSERS.split(" ")))
+AUTH = list(set(int(a) for a in Var.AUTHUSERS.split()))
 HNDLR = Var.HNDLR
 
 auth = tweepy.OAuthHandler(Var.CONSUMER_KEY, Var.CONSUMER_SECRET)
