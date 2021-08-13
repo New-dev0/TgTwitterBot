e75f79ce3957820e4865f0b7e29b391875e363df
Devesh Pal <newdev0@outlook.com>
2021-08-13 17:30:01 +0530

    Update __init__.py

--- twitterbot/__init__.py
+++ twitterbot/__init__.py
@@ -16,6 +16,7 @@ auth = tweepy.OAuthHandler(Var.CONSUMER_KEY, Var.CONSUMER_SECRET)
 auth.set_access_token(Var.ACCESS_TOKEN, Var.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+REPO = "https://github.com/New-dev0/TgTwitterBot"
 TLOGO = "https://telegra.ph/file/845054582c76963620311.jpg"
 
 HELP_MARKUP = InlineKeyboardMarkup(
